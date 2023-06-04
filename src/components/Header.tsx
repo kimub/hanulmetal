680a350661172263f47f6fc66c1ada3913951f35
kimub <kimub1204@gmail.com>
2023-06-04 16:10:05 +0900

    style: update logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,12 @@ export default function Header() {
       // ref={headerRef}
     >
       <div className='safe-area-pt-3 md:safe-area-pt-6 mx-auto flex max-w-screen-xl justify-center border-secondary px-4 pb-3 md:justify-between md:pb-6'>
-        <Image src={logo} alt='한울메탈릭스' priority />
+        <Image
+          src={logo}
+          className='w-28 md:w-40 lg:w-auto'
+          alt='한울메탈릭스'
+          priority
+        />
         <Nav />
       </div>
     </header>
