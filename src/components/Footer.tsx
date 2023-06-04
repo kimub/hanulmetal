8ad72d365102bf4d931555f171ba54ee3c6009e4
kimub <kimub1204@gmail.com>
2023-06-04 16:18:06 +0900

    style: update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,8 +9,12 @@ export default function Footer() {
             <FooterList title='상호' description='한울메탈릭스' />
             <FooterList title='대표' description='박인호' />
             <FooterList
-              title='주소'
-              description='울산 울주군 삼동면 사촌신복로 359-14'
+              title='본사(울산공장)'
+              description='울산광역시 울주군 삼동면 사촌신복로 359-14'
+            />
+            <FooterList
+              title='부산공장'
+              description='부산광역시 강서구 울만로 374-16'
             />
             <FooterList title='사업자번호' description='779-24-01630' />
             <FooterList title='copyright © 한울메탈릭스. all right reserved.' />
