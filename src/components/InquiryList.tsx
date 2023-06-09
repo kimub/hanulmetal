c636e339f6f8dcf85a9288281825fb1507d93aeb
kimub <kimub1204@gmail.com>
2023-06-10 08:49:30 +0900

    style: add width 360 under device response design

--- src/components/InquiryList.tsx
+++ src/components/InquiryList.tsx
@@ -8,11 +8,11 @@ type Props = {
 
 export default function InquiryList({ src, title, info }: Props) {
   return (
-    <li className='flex gap-8 rounded-2xl bg-white p-6 lg:w-1/3'>
-      <div className='rounded-full bg-secondary p-6'>
+    <li className='flex gap-8 rounded-2xl bg-white p-6 max-[360px]:flex-col lg:w-1/3'>
+      <div className='rounded-full bg-secondary p-6 max-[360px]:mx-auto'>
         <Image src={src} alt='title' width={40} height={40} />
       </div>
-      <div className='flex flex-col justify-center text-left'>
+      <div className='flex flex-col justify-center text-left max-[360px]:text-center'>
         <h3 className='text-lg font-bold lg:text-xl'>{title}</h3>
         <div className='mt-1 text-sm font-semibold lg:text-base'>
           {info.map((info) => (
