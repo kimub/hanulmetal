b6cd3007e3e096f5ef397c9d8be91f02d9946abe
kimub <kimub1204@gmail.com>
2023-06-21 12:00:53 +0900

    style: jsonld 주석 처리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,13 @@ import './globals.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
-const jsonLd = {
-  '@context': 'http://schema.org',
-  '@type': 'Person',
-  name: '한울메탈릭스',
-  url: 'https://www.hanulmetal.co.kr',
-  sameAs: ['https://blog.naver.com/yeon460'],
-};
+// const jsonLd = {
+//   '@context': 'http://schema.org',
+//   '@type': 'Person',
+//   name: '한울메탈릭스',
+//   url: 'https://www.hanulmetal.co.kr',
+//   sameAs: ['https://blog.naver.com/yeon460'],
+// };
 
 export default function RootLayout({
   children,
@@ -71,7 +71,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
-        <script type='application/ld+json'>{JSON.stringify(jsonLd)}</script>
+        {/* <script type='application/ld+json'>{JSON.stringify(jsonLd)}</script> */}
       </body>
     </html>
   );
